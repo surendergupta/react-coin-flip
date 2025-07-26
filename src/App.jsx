5b1e37d316a42f5bed73074c1da1028e5274174a
surendergupta <gupta.surender.1990@gmail.com>
2025-07-26 13:27:27 +0530

    empty project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <>
+      <div className='App'>
+        <h1>Welcome to Coin Flip Game</h1>
+      </div>
+    </>
+  )
+};
+
+export default App;
